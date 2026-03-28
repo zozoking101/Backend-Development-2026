e8d2936ccd511ff002b98af65d97e1a60d43dc6a
Zoë Oladokun <Zoeoladokun@gmail.com>
2026-03-28 23:53:23 +0100

    Updated README.md

--- first_backend_app/node-docker-mongo/api/src/lib/config.js
+++ first_backend_app/node-docker-mongo/api/src/lib/config.js
@@ -1,4 +1,4 @@
-import dotenv from "dotenv"
+// import dotenv from "dotenv"
 
 // Load env.development explicitly
 // dotenv.config({ path: ".env.development" }); // alternative script => nodemon --env-file=.env.development src/index.js
@@ -10,7 +10,7 @@ export default {
     prefix: '/api',
     version: '/v1',
     version_number: "1.2.0",
-    service: "User API",
+    service: "Root service",
     docs: "/docs",
     status: "ok",
 
