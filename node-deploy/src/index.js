27484dfb96d9d940bff1805e280f146406021447
Zoë Oladokun <Zoeoladokun@gmail.com>
2026-06-15 17:06:38 +0100

    fix: updated root route handler

--- node-deploy/src/index.js
+++ node-deploy/src/index.js
@@ -6,12 +6,13 @@ const PORT = 9000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    const { name } = req.body;
-    res.json({ message: `Hello ${name}` });
-});
+    const { name } = req.query;
 
+    res.json({
+        message: `Hello ${name ? name : 'World'}`
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
